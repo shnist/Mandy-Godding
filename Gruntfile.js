b4249e0fc444b24e230fb6b2615ea3677dcb3d1e
aaron faber <shnist@btinternet.com>
2013-07-10 22:18:33 +0100

    Added CI components and modified primary nav to be dynamic.. still needs some tweaking

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		clean: ['dist'],
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						src: ['index.php', 'wp-content/**'],
+						dest: '/var/www/wordpress/'
+					}
+				]
+			}
+		},
+		'ftp-deploy': {
+			build: {
+				auth: {
+					host: 'ftp.mandygodding.co.uk',
+					port: 21,
+					authKey: 'mandyGoddingKey'
+				},
+				src:'dist',
+				// .ftppass with details is needed (not included in repo)
+				dest: '/htdocs/website',
+				exclusions: ['dist/.DS_Store', 'dist/Thumbs.db', 'dist/tmp', 'dist/.git']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+
+	// Default task(s).
+	grunt.registerTask('default', ['copy']);
+	grunt.registerTask('deploy', ['ftp-deploy']);
+};
