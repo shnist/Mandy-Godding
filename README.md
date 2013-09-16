e974a1792a03a536ef9de5e415bc3f19abdd8573
shnist <aaron.jack.faber@gmail.com>
2013-09-16 13:51:28 +0100

    Removed advert

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Mandy-Godding
 =============
 
 Wordpress site for Mandy Godding's Theatre Arts
+
+
+## Adverts
+
+Adverts are contained in an overlay. Inside the overlay there is an image with the
+ID of advert. This will trigger the javascript behaviour for the advert.
+
+	<div class="overlay"></div>
+	<div class="overlayContainer">
+		<a href="#" class="close">Close</a>
+		<img id="advert" src="" alt="">
+	</div>
--- wp-content/themes/godding/loop-home.php
+++ wp-content/themes/godding/loop-home.php
@@ -1,24 +1,23 @@
-   <?php get_header(); ?>
+<?php get_header(); ?>
 
-	<?php include ('blocks/slideshow.php') ?>
-	<!-- content -->
-		<div id="cont">
-			<?php
-				$my_id = 5;
-				$post_id_5 = get_post($my_id);
-				$content = $post_id_5->post_content;
-				$content = apply_filters('the_content', $content);
-				$content = str_replace(']]>', ']]>', $content);
-				echo $content;
+<?php include ('blocks/slideshow.php') ?>
+<!-- content -->
+<div id="cont">
+	<?php
+		$my_id = 5;
+		$post_id_5 = get_post($my_id);
+		$content = $post_id_5->post_content;
+		$content = apply_filters('the_content', $content);
+		$content = str_replace(']]>', ']]>', $content);
+		echo $content;
+	?>
 
-			?>
+	<?php include('blocks/qualifications.php'); ?>
+</div>
+<!-- 		<div class="overlay"></div>
+<div class="overlayContainer">
+	<a href="#" class="close">Close</a>
+	<img id="advert" src="/wp-content/themes/godding/_images/advertisement.jpg" alt="Summer classes available. Contact us to find out more">
+</div> -->
 
-			<?php include('blocks/qualifications.php'); ?>
-		</div>
-		<div class="overlay"></div>
-		<div class="overlayContainer">
-			<a href="#" class="close">Close</a>
-			<img id="advert" src="/wp-content/themes/godding/_images/advertisement.jpg" alt="Summer classes available. Contact us to find out more">
-		</div>
-
-		<?php get_footer(); ?>
+<?php get_footer(); ?>
