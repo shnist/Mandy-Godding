93dafb975d05f5d5d98d7e916578e112eb85691e
Aaron Faber <aaron.faber@digitaslbi.com>
2015-03-27 22:21:09 +0000

    Added new slick carousel library and updated primary nav for new news item

--- wp-content/themes/godding/blocks/primary_nav.php
+++ wp-content/themes/godding/blocks/primary_nav.php
@@ -1,5 +1,6 @@
 <ul id="nav">
   <li><a href="<?php echo get_permalink( 5 ); ?>">Home</a></li>
+  <li><a href="<?php echo get_permalink( 27 ); ?>">News</a></li>
   <li><a href="<?php echo get_permalink( 9 ); ?>">Dance</a></li>
   <li><a href="<?php echo get_permalink( 12 ); ?>">Singing</a></li>
   <li><a href="<?php echo get_permalink( 14 ); ?>">Drama</a></li>
