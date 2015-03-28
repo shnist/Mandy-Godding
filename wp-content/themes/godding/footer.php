98b4476a0e3e2ac2be1ddc2025972e4ba844f955
Aaron Faber <aaron.faber@digitaslbi.com>
2015-03-28 10:02:58 +0000

    Updated footer with links and rejigged styling and layout

--- wp-content/themes/godding/footer.php
+++ wp-content/themes/godding/footer.php
@@ -1,8 +1,48 @@
-		<div id="footer">
-			<p> Mandy Godding's Theatre Arts Ltd. &copy; 2010</p>
-			<p> Established 1984 </p>
-			<p> Designed by <a href="http://www.aaronfaber.co.uk">AaronFaber</a></p>
-		</div>
+		<footer>
+			<nav class="footer-nav">
+				<h2 class="heading">Useful Links</h2>
+				<a href="<?php echo get_permalink( 5 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					Home
+				</a>
+				<a href="<?php echo get_permalink( 27 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					News
+				</a>
+				<a href="<?php echo get_permalink( 16 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					Teachers
+				</a>
+				<a href="<?php echo get_permalink( 18 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					Contact Us
+				</a>
+			</nav>
+			<nav class="footer-nav">
+				<h2 class="heading">Courses</h2>
+				<a href="<?php echo get_permalink( 9 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					Dance
+				</a>
+				<a href="<?php echo get_permalink( 12 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					Singing
+				</a>
+				<a href="<?php echo get_permalink( 14 ); ?>">
+					<i class="ion-ios-arrow-right"></i>
+					Drama
+				</a>
+				<a href="/wp-content/themes/godding/_timetable/timetable.pdf">
+					<i class="ion-ios-arrow-right"></i>
+					Timetable
+				</a>
+			</nav>
+			<?php include('blocks/qualifications.php'); ?>
+			<div class="footer-bottom">
+				<small> Mandy Godding's Theatre Arts Ltd. &copy; 2010 - 2015</small>
+				<small> Established 1984 </small>
+			</div>
+		</footer>
 		<!-- scripts -->
 		<!-- libraries -->
 		<script src="/wp-content/themes/godding/_js/jquery-1.9.1.min.js"></script>
