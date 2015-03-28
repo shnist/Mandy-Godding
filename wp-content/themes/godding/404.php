f41af379661cc0d707ee47fb0f4a141ee6f6305c
Aaron Faber <aaron.faber@digitaslbi.com>
2015-03-28 00:12:38 +0000

    Fixed 404 and index pages

--- wp-content/themes/godding/404.php
+++ wp-content/themes/godding/404.php
@@ -1,35 +1,15 @@
-   <?php get_header(); ?>
-   
-   <div id="photoShow">
-   <!--[if !IE]> -->
-	   <object type="application/x-shockwave-flash" data="/wp-content/themes/godding/_flash
-/wp-content/themes/godding/_imagesslideShowHomePage.swf" width="970" height="330">
-   <!-- <![endif]-->
-   <!--[if IE]>
-	   <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
-	   width="970" height="330">
-		   <param name="movie" value="/wp-content/themes/godding/_flash
-/wp-content/themes/godding/_imagesslideShowHomePage.swf" />
-	   <!--><!--dgx-->
-		   <param name="loop" value="true" />
-		   <param name="menu" value="false" />
-		   <param name="wmode" value="transparent" /> 
-		   <img src="/wp-content/themes/godding/_images/photoOne.png" alt="A samle photograph of the latest show">
-		   <!-- proper image fallback is needed here -->
-	   </object>
-   <!-- <![endif]-->
-   </div>
-	<!-- content -->
-		<div id="cont">
-			<h1> Sorry, something has gone wrong (404) </h1>
-			<p>
-				The page you are looking for is currently lost. To get back on track, why not
-				<a href="/"> return to our homepage? </a>
-			</p>
-			
-			<!-- logos -->
-			<img class="orgLogos" src="/wp-content/themes/godding/_images/bboLogo.jpg" alt="British Ballet Organisation" />
-			<img class="orgLogos" src="/wp-content/themes/godding/_images/istdLogo.jpg" alt="Imperial Society of Teachers of Dancing" />
-		</div>
-		
-		<?php get_footer(); ?>
+<?php get_header(); ?>
+
+<?php include ('blocks/slideshow.php') ?>
+<!-- content -->
+<div id="cont">
+	<h1> Sorry, something has gone wrong (404) </h1>
+	<p>
+		The page you are looking for is currently lost. To get back on track, why not
+		<a href="/"> return to our homepage? </a>
+	</p>
+
+	<?php include('blocks/qualifications.php'); ?>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/godding/index.php
+++ wp-content/themes/godding/index.php
@@ -1,29 +1,10 @@
-   <?php get_header(); ?>
+<?php get_header(); ?>
 
-   <div id="photoShow">
-   <!--[if !IE]> -->
-	   <object type="application/x-shockwave-flash" data="/wp-content/themes/godding/_flash
-/wp-content/themes/godding/_imagesslideShowHomePage.swf" width="970" height="330">
-   <!-- <![endif]-->
-   <!--[if IE]>
-	   <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
-	   width="970" height="330">
-		   <param name="movie" value="/wp-content/themes/godding/_flash
-/wp-content/themes/godding/_imagesslideShowHomePage.swf" />
-	   <!--><!--dgx-->
-		   <param name="loop" value="true" />
-		   <param name="menu" value="false" />
-		   <param name="wmode" value="transparent" />
-		   <img src="/wp-content/themes/godding/_images/photoOne.png" alt="A sample photograph of the latest show">
-		   <!-- proper image fallback is needed here -->
-	   </object>
-   <!-- <![endif]-->
-   </div>
-	<!-- content -->
-		<div id="cont">
-			<!-- logos -->
-			<img class="orgLogos" src="/wp-content/themes/godding/_images/bboLogo.jpg" alt="British Ballet Organisation" />
-			<img class="orgLogos" src="/wp-content/themes/godding/_images/istdLogo.jpg" alt="Imperial Society of Teachers of Dancing" />
-		</div>
+<?php include ('blocks/slideshow.php') ?>
 
-		<?php get_footer(); ?>
+<!-- content -->
+<div id="cont">
+	<?php include('blocks/qualifications.php'); ?>
+</div>
+
+<?php get_footer(); ?>
