c7d35cdc1520564e90f2c34d428d378358d427ac
Aaron Faber <aaron.faber@digitaslbi.com>
2015-03-29 09:32:04 +0100

    Updated print styles for timetable and added some useful generic stuff

--- wp-content/themes/godding/header.php
+++ wp-content/themes/godding/header.php
@@ -65,7 +65,7 @@
 			<div id="header">
 				<?php include ('blocks/logo.php'); ?>
 				<a href="/index.html">
-					<img id="mgtaLogo" src="/wp-content/themes/godding/_images/title.png" alt="Mandy Godding's Theatre Arts Logo" width="557" height="106">
+					<img id="mgtaLogo" src="/wp-content/themes/godding/_images/title.png" alt="Mandy Godding's Theatre Arts Logo">
 				</a>
 				<?php include ('blocks/primary_nav.php'); ?>
 			</div>
