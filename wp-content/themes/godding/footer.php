b0a1e2e855dcc1d1f8c84bee7b1e9d9974791c5d
Aaron Faber <aaron.faber@digitaslbi.com>
2015-03-31 23:03:55 +0100

    Removed swf object and added fixed width to iframes

--- wp-content/themes/godding/footer.php
+++ wp-content/themes/godding/footer.php
@@ -9,7 +9,7 @@
 		<script src="/wp-content/themes/godding/_js/jquery-ui-1.8.16.custom.min.js"></script>
 		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
 		<!-- custom -->
-		<script src="/wp-content/themes/godding/_js/swf_object.js"></script>
+		
 		<script src='/wp-content/themes/godding/_js/slick.js'></script>
 		<script src='/wp-content/themes/godding/_js/common.js'></script>
 	</body>
