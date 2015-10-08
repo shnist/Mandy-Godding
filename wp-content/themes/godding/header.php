d8873440eef11a59d78cdbd320e8374b730be8e1
Aaron Faber <aaron.faber@digitaslbi.com>
2015-10-08 15:32:16 +0100

    Facebook integration

--- wp-content/themes/godding/header.php
+++ wp-content/themes/godding/header.php
@@ -66,9 +66,9 @@
 			<!-- facebook integration -->
 			<div id="fb-root"></div>
 			<script>
-				(function(d, s, id) {
+				(function(d, s, id){
 					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) return;
+					if (d.getElementById(id)) {return;}
 					js = d.createElement(s); js.id = id;
 					js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=801634389905442";
 					fjs.parentNode.insertBefore(js, fjs);
