0706df6f2907204004acd5af862785775cd4f340
Aaron Faber <aaron.faber@iiincubator.com>
2018-09-05 11:52:23 +0100

    New primary nav set up

--- wp-content/themes/godding/functions.php
+++ wp-content/themes/godding/functions.php
@@ -1,6 +1,15 @@
-<?php 
-add_action('get_header', 'remove_admin_login_header');
-function remove_admin_login_header() {
-	remove_action('wp_head', '_admin_bar_bump_cb');
+<?php
+// add_action('get_header', 'remove_admin_login_header');
+
+// function remove_admin_login_header() {
+// 	remove_action('wp_head', '_admin_bar_bump_cb');
+// }
+
+## Registers navigation menus
+function register_my_menus() {
+	register_nav_menus(array(
+    'primary-nav' => __( 'Primary Nav' ),
+  ));
 }
+add_action( 'init', 'register_my_menus' );
 ?>
