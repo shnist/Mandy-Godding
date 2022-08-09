7c42b62262c8e127ba15834c8cfb76e75f5e72e9
Aaron Faber <483733+shnist@users.noreply.github.com>
2022-08-09 17:18:19 +0100

    added new logo to header

--- themes/godding/blocks/logo.php
+++ themes/godding/blocks/logo.php
@@ -1,1 +1,1 @@
-<a href="/"><img src="/wp-content/themes/godding/_images/mgta-logo.png" alt="Mandy Godding's Theatre Arts"></a>
+<a href="/"><img src="/wp-content/themes/godding/_images/mgta-logo.jpeg" alt="Mandy Godding's Theatre Arts"></a>
