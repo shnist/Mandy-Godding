282a6e933b8d725f351fdf58a5448ed7131f78fa
Aaron Faber <483733+shnist@users.noreply.github.com>
2022-08-10 11:04:56 +0100

    Updated template based solution

---
+++ themes/godding/templates/template-default.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: Default template
+ */
+
+get_header();
+
+?>
+
+<?php
+if (have_posts()) {
+
+  while (have_posts()) {
+    the_post();
+
+    get_template_part('template-parts/content', 'page');
+  }
+}
+?>
+
+
+<?php get_footer(); ?>
